a9c1148dd936561b2492ccb46d4e1a5d74133875
Oliver Lipkau <oliver@lipkau.net>
2020-08-29 01:54:30 +0200

    Changed testing to be in the module's scope
    as Get-BaseHost uses module scoped variables,
    the tests have been changed so that the function
    is tested as such

--- cf-api/Private/Get-BaseHost.Tests.ps1
+++ cf-api/Private/Get-BaseHost.Tests.ps1
@@ -1,21 +1,23 @@
 Set-StrictMode -Version Latest
 
 BeforeAll {
-    . "$PSScriptRoot\Get-BaseHost.ps1"
+    Remove-Module "cf-api"
+    Import-Module (Join-Path $PSScriptRoot "../cf-api.psd1")
 }
 Describe "Get-BaseHost" {
-    Context "Access Script Level Variables" {
-        It "throws exceptions when script level variable is not set" {
-            { Get-BaseHost } | Should -Throw "*cannot be retrieved because it has not been set*"
-        }
-        It "is set" {
-            $hostUrl = "http://localhost"
-            Set-Variable -Scope Script -Name baseHost -Value $hostUrl
-            Get-BaseHost | Should -Be $hostUrl
-        }
-        It "is null" {
-            Set-Variable -Scope Script -Name baseHost -Value $null
-            { Get-BaseHost } | Should -Throw "*baseHost is not set in script variable*"
+    InModuleScope "cf-api" {
+        Context "Access Script Level Variables" {
+            It "throws exceptions when script level variable is not set" {
+                { Get-BaseHost } | Should -Throw "*baseHost is not set in script variable*"
+            }
+            It "is set" {
+                $script:baseHost = "http://localhost"
+                Get-BaseHost | Should -Be "http://localhost"
+            }
+            It "is null" {
+                $script:baseHost = $null
+                { Get-BaseHost } | Should -Throw "*baseHost is not set in script variable*"
+            }
         }
     }
 }
