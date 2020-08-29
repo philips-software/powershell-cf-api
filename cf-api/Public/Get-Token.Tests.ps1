2f8cb4b801b1af7d8a9e38e8225da6e8037d6126
Oliver Lipkau <oliver@lipkau.net>
2020-08-29 13:10:43 +0200

    Added tests for `Get-Token`

--- cf-api/Public/Get-Token.Tests.ps1
+++ cf-api/Public/Get-Token.Tests.ps1
@@ -1,86 +1,69 @@
-# Set-StrictMode -Version Latest
+Set-StrictMode -Version Latest
 
-# BeforeAll {
-#     . "$PSScriptRoot\Get-Token.ps1"
-#     . "$PSScriptRoot\Invoke-Retry.ps1"
-#     . "$PSScriptRoot\..\Private\Compare-HashTable.ps1"
-#     . "$PSScriptRoot\..\Private\PesterMatchHashtable.Tests.ps1"
-# }
+BeforeAll {
+    . "$PSScriptRoot\Get-Token.ps1"
+    . "$PSScriptRoot\Invoke-Retry.ps1"
+}
 
 
-# Describe "Get-Token" {
-#     BeforeAll {
-#         [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignment', '', Justification='pester supported')]
-#         $params = @{
-#             Username = "user1"
-#             Password = "pass1"
-#             CloudFoundryAPI = "http://cf.com"
-#         }
-#     }
-#     Context "API call" {
-#         It "is called with the correct URLs" {
-#             $Content = "{'name': 'foo'}"
-#             Mock -Command Invoke-WebRequest {
-#                 if ($Uri -eq "$($CloudFoundryAPI)/v2/info") {
-#                     return @{
-#                         StatusCode=200
-#                         Content="{'authorization_endpoint': 'http://auth'}"
-#                     }
-#                 }
-#                 if ($Uri -Match "http://auth/oauth/token") {
-#                     return @{
-#                         StatusCode=200
-#                         Content=$Content
-#                     }
-#                 } `
-#             }
-#             (Get-Token @params) | Should MatchHashtable $Content
-#             Should -Invoke Invoke-WebRequest -ParameterFilter {
-#                 $matchHeader = @{
-#                     "Authorization"="Basic Y2Y6"
-#                     "Accept"="application/json"
-#                     "Content-Type"="application/x-www-form-urlencoded; charset=UTF-8"
-#                 }
-#                 if ($Uri -eq "$($CloudFoundryAPI)/v2/info") {
-#                     return $Method -eq "Get" -and (Compare-Hashtable $Header $matchHeader) -eq $null
-#                 }
-#                 if ($Uri -Match "http://auth/oauth/token") {
-#                     return $Method -eq "Post" -and (Compare-HashTable $Header $matchHeader) -eq $null -and $Body -eq "grant_type=password&password=$($Password)&scope=&username=$($Username)"
-#                 }
-#                 $false
-#             }
-#         }
-#     }
-#     Context  "failed API call" {
-#         It "fails" {
-#             Mock -Command Invoke-WebRequest {
-#                 if ($Uri -eq "$($CloudFoundryAPI)/v2/info") {
-#                     return @{
-#                         StatusCode=400
-#                     }
-#                 }
-#             }
-#             {Get-Token @params} | Should -Throw "400"
-#         }
-#     }
-#     Context "fail API auth" {
-#         It "fail" {
-#             $Content = "{'name': 'foo'}"
-#             Mock -Command Invoke-WebRequest {
-#                 if ($Uri -eq "$($CloudFoundryAPI)/v2/info") {
-#                     return @{
-#                         StatusCode=200
-#                         Content="{'authorization_endpoint': 'http://auth'}"
-#                     }
-#                 }
-#                 if ($Uri -Match "http://auth/oauth/token") {
-#                     return @{
-#                         StatusCode=400
-#                         Content=$Content
-#                     }
-#                 } `
-#             }
-#             {Get-Token @params} | Should -Throw "400"
-#         }
-#     }
-# }
+Describe "Get-Token" {
+    BeforeAll {
+        [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignment', '', Justification = 'pester supported')]
+        $params = @{
+            Username        = "user1"
+            Password        = "pass1"
+            CloudFoundryAPI = "http://cf.com"
+        }
+
+        Mock Invoke-WebRequest -ParameterFilter { $Uri -like "*/v2/info" } -Verifiable {
+            @{ StatusCode = 200; Content = "{'authorization_endpoint': 'http://auth'}" }
+        }
+        Mock Invoke-WebRequest -ParameterFilter { $Uri -like "*/oauth/token" } -Verifiable {
+            @{ StatusCode = 200; Content = '{"name":"foo"}' }
+        }
+        Mock Invoke-WebRequest { throw "this mock should never be called" }
+    }
+    Context "API call" {
+        It "authenticates with username and password against the api" {
+            Get-Token @params | ConvertTo-Json -Compress | Should -Be '{"name":"foo"}'
+
+            Assert-MockCalled Invoke-WebRequest -ParameterFilter { $Uri -like "*/v2/info" } -Exactly -Times 1 -Scope It
+            Assert-MockCalled Invoke-WebRequest -ParameterFilter { $Uri -like "*/oauth/token" } -Exactly -Times 1 -Scope It
+
+            Assert-MockCalled Invoke-WebRequest -ParameterFilter {
+                $Headers["Authorization"] -eq "Basic Y2Y6"
+                $Headers["Accept"] -eq "application/json"
+                $Headers["Content-Type"] -eq "application/x-www-form-urlencoded; charset=UTF-8"
+            } -Exactly -Times 2 -Scope It
+
+            Assert-MockCalled Invoke-WebRequest -ParameterFilter {
+                $Body -eq "grant_type=password&password=pass1&scope=&username=user1"
+            } -Exactly -Times 1 -Scope It
+        }
+
+        It "authenticates with passcode against the api" {
+            Get-Token -Passcode "XXXXXXXX" -CloudFoundryAPI "http://cf.com" -ErrorAction Stop
+
+            Assert-MockCalled Invoke-WebRequest -ParameterFilter { $Uri -like "*/v2/info" } -Exactly -Times 1 -Scope It
+            Assert-MockCalled Invoke-WebRequest -ParameterFilter { $Uri -like "*/oauth/token" } -Exactly -Times 1 -Scope It
+
+            Assert-MockCalled Invoke-WebRequest -ParameterFilter {
+                $Body -eq "grant_type=password&passcode=XXXXXXXX&token_format=jwt"
+            } -Exactly -Times 1 -Scope It
+        }
+    }
+    It "returns an error when the info endpoint can't be called" {
+        Mock Invoke-WebRequest -ParameterFilter { $Uri -like "*/v2/info" } -Verifiable {
+            @{ StatusCode = 400 }
+        }
+
+        { Get-Token @params -ErrorAction Stop } | Should -Throw "Failed when calling 'http://cf.com/v2/info' with status code 400"
+    }
+    It "returns an error when no auth token can be retrieved" {
+        Mock Invoke-WebRequest -ParameterFilter { $Uri -like "*/oauth/token" } -Verifiable {
+            @{ StatusCode = 400; Content = '{"name":"foo"}' }
+        }
+
+        { Get-Token @params -ErrorAction Stop } | Should -Throw "Failed when calling 'http://auth/oauth/token' with status code 400"
+    }
+}
