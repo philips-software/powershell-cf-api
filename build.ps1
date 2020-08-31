f20f808243f02f0e333e986959ee3712d1785de4
viperx77 <lindell.mark@gmail.com>
2020-08-31 13:26:46 -0400

    fix build

--- build.ps1
+++ build.ps1
@@ -7,8 +7,11 @@ trap {
 }
 $ErrorActionPreference = "Stop"
 
-# ensure the module imports
-Import-Module -Name ./cf-api -Force
+# run unit tests
+Push-Location $PSScriptRoot/cf-api
+    # ensure the module imports
+    Import-Module -Name ./cf-api -Force
+Pop-Location
 
 if ($Test) {
     & .\cf-api.tests.ps1
@@ -17,17 +20,16 @@ if ($Test) {
 # run unit tests
 Push-Location $PSScriptRoot/cf-api
 
-if ($Analyze) {
-    Install-Module -Name PSScriptAnalyzer -Force
-    Invoke-ScriptAnalyzer -Path Public -Recurse # -EnableExit <= add once all warnings are cleaned up
-}
+    if ($Analyze) {
+        Install-Module -Name PSScriptAnalyzer -Force
+        Invoke-ScriptAnalyzer -Path Public -Recurse # -EnableExit <= add once all warnings are cleaned up
+    }
 
-$semver = "$($major).$($minor).$($patch)"
-Write-Verbose "Setting version to $($semver)"
+    $semver = "$($major).$($minor).$($patch)"
+    Write-Verbose "Setting version to $($semver)"
 
-((Get-Content -path cf-api-template.nuspec -Raw) -replace '\${NUGET_VERSION}',$semver) | Set-Content -Path cf-api.nuspec
-((Get-Content -path cf-api-template.psd1 -Raw) -replace '\${NUGET_VERSION}',$semver) | Set-Content -Path cf-api.psd1
-
+    ((Get-Content -path cf-api-template.nuspec -Raw) -replace '\${NUGET_VERSION}',$semver) | Set-Content -Path cf-api.nuspec
+    ((Get-Content -path cf-api-template.psd1 -Raw) -replace '\${NUGET_VERSION}',$semver) | Set-Content -Path cf-api.psd1
 Pop-Location
 
 exit 0
